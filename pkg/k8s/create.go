5eec5de8e71b74c39bc42bfa3fc584cb1d78af1f
Suresh Khatri <servlet2@msn.com>
2022-02-27 16:35:33 +1100

    nodegroup with managed option

--- pkg/k8s/create.go
+++ pkg/k8s/create.go
@@ -3,6 +3,8 @@ package k8s
 import (
 	"encoding/json"
 	"fmt"
+	eks2 "github.com/pulumi/pulumi-aws/sdk/v4/go/aws/eks"
+	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/iam"
 	"github.com/pulumi/pulumi-eks/sdk/go/eks"
 	k8s "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -17,6 +19,7 @@ type NodeGroupParams struct {
 	SpotPrice    string            `json:"spotPrice" yaml:"spotPrice"`
 	PublicKey    bool              `json:"publicKey" yaml:"publicKey"`
 	PublicIp     *bool             `json:"publicIp" yaml:"publicIp"`
+	Managed      *bool             `json:"managed" yaml:"managed"`
 }
 
 type EksCluster struct {
@@ -32,13 +35,20 @@ type EksCluster struct {
 }
 
 func CreateEksCluster(ctx *pulumi.Context, clusterParams EksCluster) error {
-	clusterArgs := eks.ClusterArgs{
+	err, role1, instanceProfile1 := createRoleAndInstanceProfile(ctx)
+	if err != nil {
+		return err
+	}
+
+	var clusterArgs *eks.ClusterArgs
+
+	fmt.Println("total node groups", len(clusterParams.NodeGroups))
+	clusterArgs = &eks.ClusterArgs{
 		Name: pulumi.String(clusterParams.Name),
 	}
 	if clusterParams.Version != "" {
 		clusterArgs.Version = pulumi.String(clusterParams.Version)
 	}
-	fmt.Println("total node groups", len(clusterParams.NodeGroups))
 	if clusterParams.Size != nil && *clusterParams.Size > 0 {
 		size := *clusterParams.Size
 		clusterArgs.DesiredCapacity = pulumi.Int(size)
@@ -49,8 +59,11 @@ func CreateEksCluster(ctx *pulumi.Context, clusterParams EksCluster) error {
 			instanceType = *clusterParams.InstanceType
 		}
 		clusterArgs.InstanceType = pulumi.String(instanceType)
+		clusterArgs.InstanceRoles = iam.RoleArray{role1}
+		clusterArgs.InstanceProfileName = instanceProfile1.Name
+	} else {
+		clusterArgs.SkipDefaultNodeGroup = pulumi.Bool(true)
 	}
-
 	if clusterParams.VpcId != "" {
 		clusterArgs.VpcId = pulumi.String(clusterParams.VpcId)
 	}
@@ -74,13 +87,15 @@ func CreateEksCluster(ctx *pulumi.Context, clusterParams EksCluster) error {
 	}
 	clusterArgs.NodeAssociatePublicIpAddress = pulumi.Bool(false)
 
-	cluster, err := eks.NewCluster(ctx, clusterParams.Name, &clusterArgs)
+	fmt.Println("cluster args", clusterArgs)
+	cluster, err := eks.NewCluster(ctx, clusterParams.Name, clusterArgs)
 
 	if err != nil {
 		return err
 	}
 
 	eksProvider, err := k8s.NewProvider(ctx, "eksProvider", &k8s.ProviderArgs{
+		Cluster: clusterArgs.Name,
 		Kubeconfig: cluster.Kubeconfig.ApplyT(
 			func(config interface{}) (string, error) {
 				b, err := json.Marshal(config)
@@ -93,30 +108,110 @@ func CreateEksCluster(ctx *pulumi.Context, clusterParams EksCluster) error {
 	if err != nil {
 		return err
 	}
+
 	eksProviders := pulumi.ProviderMap(map[string]pulumi.ProviderResource{
-		"kubernetes": eksProvider,
+		"provider": eksProvider,
 	})
+
+	err = createNodeGroups(ctx, clusterParams, cluster, instanceProfile1, eksProviders)
+	if err != nil {
+		return err
+	}
+
+	err = createManagedNodeGroups(ctx, clusterParams, cluster, role1, eksProviders)
+	if err != nil {
+		return err
+	}
+
+	ctx.Export("kubeconfig", cluster.Kubeconfig)
+	return err
+}
+
+func createRoleAndInstanceProfile(ctx *pulumi.Context) (error, *iam.Role, *iam.InstanceProfile) {
+	tmpJSON0, err := json.Marshal(map[string]interface{}{
+		"Statement": []map[string]interface{}{
+			map[string]interface{}{
+				"Action": "sts:AssumeRole",
+				"Effect": "Allow",
+				"Principal": map[string]interface{}{
+					"Service": "ec2.amazonaws.com",
+				},
+			},
+		},
+		"Version": "2012-10-17",
+	})
+	if err != nil {
+		return err, nil, nil
+	}
+	json0 := string(tmpJSON0)
+	role1, err := iam.NewRole(ctx, "example", &iam.RoleArgs{
+		AssumeRolePolicy: pulumi.String(json0),
+	})
+	if err != nil {
+		return err, nil, nil
+	}
+	_, err = iam.NewRolePolicyAttachment(ctx, "example-AmazonEKSWorkerNodePolicy", &iam.RolePolicyAttachmentArgs{
+		PolicyArn: pulumi.String("arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"),
+		Role:      role1.Name,
+	})
+	if err != nil {
+		return err, nil, nil
+	}
+	_, err = iam.NewRolePolicyAttachment(ctx, "example-AmazonEKSCNIPolicy", &iam.RolePolicyAttachmentArgs{
+		PolicyArn: pulumi.String("arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"),
+		Role:      role1.Name,
+	})
+	if err != nil {
+		return err, nil, nil
+	}
+	_, err = iam.NewRolePolicyAttachment(ctx, "example-AmazonEC2ContainerRegistryReadOnly", &iam.RolePolicyAttachmentArgs{
+		PolicyArn: pulumi.String("arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"),
+		Role:      role1.Name,
+	})
+	if err != nil {
+		return err, nil, nil
+	}
+	var instanceProfile1 *iam.InstanceProfile
+	instanceProfile1, err = iam.NewInstanceProfile(ctx, "testProfile", &iam.InstanceProfileArgs{
+		Role: role1.Name,
+	})
+	return err, role1, instanceProfile1
+}
+
+func createNodeGroups(ctx *pulumi.Context, clusterParams EksCluster, cluster *eks.Cluster, instanceProfile1 *iam.InstanceProfile, eksProviders pulumi.ResourceOption) error {
 	for _, nodeGroupParam := range clusterParams.NodeGroups {
-		nodeGroupArgs := eks.NodeGroupArgs{
-			InstanceType:       pulumi.String(nodeGroupParam.InstanceType),
-			DesiredCapacity:    pulumi.Int(nodeGroupParam.Size),
-			MinSize:            pulumi.Int(nodeGroupParam.Size),
-			MaxSize:            pulumi.Int(nodeGroupParam.Size),
-			Cluster:            cluster.Core,
-			Version:            pulumi.String(clusterParams.Version),
-			NodeRootVolumeSize: pulumi.Int(100),
+		if nodeGroupParam.Managed != nil && *nodeGroupParam.Managed {
+			continue
+		}
+		nodeGroupsArgs := eks.NodeGroupArgs{
+			Cluster:         cluster.Core,
+			Labels:          pulumi.StringMap{"topology.kubernetes.io/node-group-name": pulumi.String(nodeGroupParam.Name)},
+			InstanceType:    pulumi.String(nodeGroupParam.InstanceType),
+			DesiredCapacity: pulumi.Int(nodeGroupParam.Size),
+			MinSize:         pulumi.Int(0),
+			MaxSize:         pulumi.Int(nodeGroupParam.Size),
+
+			InstanceProfile:    instanceProfile1,
 			NodeSubnetIds:      pulumi.ToStringArray(strings.Split(clusterParams.SubnetId, ",")),
-			Labels:             pulumi.StringMap{"topology.kubernetes.io/node-group-name": pulumi.String(nodeGroupParam.Name)},
+			NodeRootVolumeSize: pulumi.Int(100),
+			NodePublicKey:      pulumi.String(clusterParams.NodePublicKey),
+			Version:            pulumi.String(clusterParams.Version),
 		}
 
 		if clusterParams.NodePublicKey != "" && nodeGroupParam.PublicKey {
-			nodeGroupArgs.NodePublicKey = pulumi.String(clusterParams.NodePublicKey)
+			nodeGroupsArgs.NodePublicKey = pulumi.String(clusterParams.NodePublicKey)
 		}
-		taintMap := make(map[string]eks.TaintInput, 0)
 
+		taintMap := make(map[string]eks.TaintInput, 0)
+		taintMap["node.kubernetes.io/not-ready"] = eks.TaintArgs{
+			Effect: pulumi.String("NO_SCHEDULE"),
+		}
+		taintMap["node.kubernetes.io/unreachable"] = eks.TaintArgs{
+			Effect: pulumi.String("NO_SCHEDULE"),
+		}
 		for k, v := range nodeGroupParam.Taint {
 			parts := strings.Split(v, ":")
-			effect := "PreferNoSchedule"
+			effect := "PREFER_NO_SCHEDULE"
 			if len(parts) == 2 {
 				effect = parts[1]
 			}
@@ -125,16 +220,71 @@ func CreateEksCluster(ctx *pulumi.Context, clusterParams EksCluster) error {
 				Effect: pulumi.String(effect),
 			}
 		}
-		nodeGroupArgs.Taints = eks.TaintMap(taintMap)
-		publicIp := nodeGroupParam.PublicIp != nil && *nodeGroupParam.PublicIp
-		nodeGroupArgs.NodeAssociatePublicIpAddress = pulumi.Bool(publicIp)
-		_, nodeGroupErr := eks.NewNodeGroup(ctx, nodeGroupParam.Name, &nodeGroupArgs, eksProviders)
+		nodeGroupsArgs.Taints = eks.TaintMap(taintMap)
+
+		_, nodeGroupErr := eks.NewNodeGroup(ctx, nodeGroupParam.Name, &nodeGroupsArgs, eksProviders)
 		if nodeGroupErr != nil {
 			return nodeGroupErr
 		}
 	}
+	return nil
+}
 
-	ctx.Export("kubeconfig", cluster.Kubeconfig)
+func createManagedNodeGroups(ctx *pulumi.Context, clusterParams EksCluster, cluster *eks.Cluster, role1 *iam.Role, eksProviders pulumi.ResourceOption) error {
+	for _, nodeGroupParam := range clusterParams.NodeGroups {
+		if nodeGroupParam.Managed == nil || !*nodeGroupParam.Managed {
+			continue
+		}
+		managedNodeGroupArgs := eks.ManagedNodeGroupArgs{
+			Cluster:       cluster.Core,
+			Version:       pulumi.String(clusterParams.Version),
+			Labels:        pulumi.StringMap{"topology.kubernetes.io/node-group-name": pulumi.String(nodeGroupParam.Name)},
+			InstanceTypes: pulumi.StringArray{pulumi.String(nodeGroupParam.InstanceType)},
+			ScalingConfig: eks2.NodeGroupScalingConfigArgs{
+				DesiredSize: pulumi.Int(nodeGroupParam.Size),
+				MinSize:     pulumi.Int(0),
+				MaxSize:     pulumi.Int(nodeGroupParam.Size),
+			},
+			NodeGroupName: pulumi.String(nodeGroupParam.Name),
+			DiskSize:      pulumi.Int(100),
+			SubnetIds:     pulumi.ToStringArray(strings.Split(clusterParams.SubnetId, ",")),
+			NodeRole:      role1,
+		}
 
-	return err
+		if clusterParams.NodePublicKey != "" && nodeGroupParam.PublicKey {
+			managedNodeGroupArgs.RemoteAccess = eks2.NodeGroupRemoteAccessArgs{
+				Ec2SshKey: pulumi.String(clusterParams.NodePublicKey),
+			}
+		}
+		taintArray := eks2.NodeGroupTaintArray{
+			eks2.NodeGroupTaintArgs{
+				Key:    pulumi.String("node.kubernetes.io/not-ready"),
+				Effect: pulumi.String("NO_SCHEDULE"),
+			},
+			eks2.NodeGroupTaintArgs{
+				Key:    pulumi.String("node.kubernetes.io/unreachable"),
+				Effect: pulumi.String("NO_SCHEDULE"),
+			},
+		}
+
+		for k, v := range nodeGroupParam.Taint {
+			parts := strings.Split(v, ":")
+			effect := "PREFER_NO_SCHEDULE"
+			if len(parts) == 2 {
+				effect = parts[1]
+			}
+			taintArray = append(taintArray, eks2.NodeGroupTaintArgs{
+				Key:    pulumi.String(k),
+				Value:  pulumi.String(parts[0]),
+				Effect: pulumi.String(effect),
+			})
+		}
+		managedNodeGroupArgs.Taints = taintArray
+
+		_, nodeGroupErr := eks.NewManagedNodeGroup(ctx, nodeGroupParam.Name, &managedNodeGroupArgs, eksProviders)
+		if nodeGroupErr != nil {
+			return nodeGroupErr
+		}
+	}
+	return nil
 }
